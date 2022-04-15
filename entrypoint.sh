d70608d88d3bf9bccc25e5050d50a1eb94ba18d1
Lars Gohr <lars@gohr.digital>
2022-04-15 20:33:16 +0200

    :recycle: Add deprecation warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo ">> elgohr/Github-Release-Action@master is deprecated!"
+echo ">> Please switch to elgohr/Github-Release-Action@v4"
 
 MESSAGE=$*
 hub release create -m ${MESSAGE} $(date +%Y%m%d%H%M%S)
