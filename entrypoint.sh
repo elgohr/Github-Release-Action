d32e8668bfc11afc3b0ef9fb94c7f54ad5b891ce
Kroese <kroese@users.noreply.github.com>
2023-04-29 03:44:35 +0200

    Support custom tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-hub release create -m "${INPUT_TITLE}" "$(date +%Y%m%d%H%M%S)"
+
+if [ -z "${INPUT_TAG}" ] 
+  INPUT_TAG="$(date +%Y%m%d%H%M%S)"
+fi
+
+hub release create -m "${INPUT_TITLE}" "${INPUT_TAG}" 
