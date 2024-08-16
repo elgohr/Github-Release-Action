bf0f5ddb70704b9003696710f9e327b96abcbdb1
Sean Smith <seanwessmith@gmail.com>
2024-08-16 12:32:44 -0700

    fix: fixed bug with tag. added tag to input and added default tag to entrypoint incase a tag was not provided

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ if [ ! -z "${INPUT_WORKDIR}" ]; then
     cd "${INPUT_WORKDIR}"
 fi
 
-gh release create $INPUT_TAG -t "${INPUT_TITLE}" "$(date +%Y%m%d%H%M%S)" --generate-notes
+# Generate a tag if not provided
+if [ -z "${INPUT_TAG}" ]; then
+    INPUT_TAG="release-$(date +%Y%m%d%H%M%S)"
+    echo "No tag provided. Generated tag: ${INPUT_TAG}"
+fi
+
+# Create the release
+gh release create "${INPUT_TAG}" -t "${INPUT_TITLE}" --generate-notes
