848c56db2175ed5420f0c81e94d27a017ae63d92
Sean Smith <seanwessmith@users.noreply.github.com>
2024-08-16 12:17:46 -0700

    fix: reduced to just tag, saving notes for future release with testing (#56)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ ! -z "${INPUT_WORKDIR}" ]; then
     cd "${INPUT_WORKDIR}"
 fi
 
-gh release create -t "${INPUT_TITLE}" "$(date +%Y%m%d%H%M%S)" --generate-notes
+gh release create $INPUT_TAG -t "${INPUT_TITLE}" "$(date +%Y%m%d%H%M%S)" --generate-notes
