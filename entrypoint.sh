6c22de035417e6eaa50d36f9267ef1e8a1d8899d
Lars Gohr <lars@gohr.digital>
2024-08-25 19:39:26 +0200

    🐛 Prevent double tag on tagged releases. Introduce integration test for tagged releases. Thank you @seanwessmith for the hint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ if [ ! -z "${INPUT_WORKDIR}" ]; then
     cd "${INPUT_WORKDIR}"
 fi
 
-gh release create $INPUT_TAG -t "${INPUT_TITLE}" "$(date +%Y%m%d%H%M%S)" --generate-notes
+if [ -z "${INPUT_TAG}" ]; then
+    INPUT_TAG="release-$(date +%Y%m%d%H%M%S)"
+fi
+
+gh release create $INPUT_TAG -t "${INPUT_TITLE}" --generate-notes
